c8b5b0717557290576286c90ef139a4e00c48b91
ct595717 <ct85711@gmail.com>
2017-02-22 12:38:08 -0600

    Initial Commit, Setting up repo with existing project.

---
+++ SimpleAI/SimpleAI/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleAI
+{
+    public partial class Form1 : Form
+    {
+        private int xPos = 20;
+        private int yPos = 20;
+        const int WinWidth = 640;
+        const int WinHeight = 480;
+        const int boundary = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Box.Location = new Point(xPos, yPos);
+            while (true)
+            {
+                Movement();
+
+                Box.Location = new Point(xPos, yPos);
+
+            }
+        }
+
+        private void Movement()
+        {
+
+        }
+    }
+}
